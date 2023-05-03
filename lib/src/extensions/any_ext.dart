d7db2005a62affd6edca3e65531d2ef643ed8f94
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2023-05-03 14:30:16 +0300

    * removed 'to'

--- lib/src/extensions/any_ext.dart
+++ lib/src/extensions/any_ext.dart
@@ -1,8 +1,6 @@
 extension AnyExt<T> on T {
   X let<X>(X Function(T it) block) => block.call(this);
 
-  X to<X>(X value) => value;
-
   T use(void Function(T it) block) {
     block.call(this);
     return this;
