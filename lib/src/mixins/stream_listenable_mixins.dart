c36373ade677213bce8468cba3a9075351d90753
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2023-05-11 10:53:02 +0300

    no message

--- lib/src/mixins/stream_listenable_mixins.dart
+++ lib/src/mixins/stream_listenable_mixins.dart
@@ -5,7 +5,7 @@ import 'package:siberian_core/src/functions.dart';
 mixin StreamListenableMixin {
   final List<StreamSubscription> _subscriptions = [];
 
-  void listenToStream<T extends Object>(Stream<T> stream, TypedCallback<T> onChange) {
+  void listenToStream<T extends Object?>(Stream<T> stream, TypedCallback<T> onChange) {
     _subscriptions.add(stream.listen(onChange));
   }
 
