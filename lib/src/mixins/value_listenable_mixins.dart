e2f3bb5ede420168b3350f9dbd31e4158874d275
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2023-05-11 17:27:20 +0300

    no message

--- lib/src/mixins/value_listenable_mixins.dart
+++ lib/src/mixins/value_listenable_mixins.dart
@@ -4,17 +4,23 @@ import 'package:siberian_core/src/functions.dart';
 mixin ValueListenableMixin {
   final Map<ValueListenable, List<VoidCallback>> _valueListenableSubscriptions = {};
 
-  void onValueChanged<T extends Object>(ValueListenable<T> valueListenable, TypedCallback<T> onChange) {
+  void listenToValueChanges<T extends Object>(ValueListenable<T> valueListenable, TypedCallback<T> onChange) {
     listener() => onChange(valueListenable.value);
     _valueListenableSubscriptions.putIfAbsent(valueListenable, () => []).add(listener);
     valueListenable.addListener(listener);
   }
 
-  void removeListeners() {
+  @Deprecated("use listenToValueChanges")
+  void onValueChanged<T extends Object>(ValueListenable<T> valueListenable, TypedCallback<T> onChange) => listenToValueChanges;
+
+  void cancelValueListeners() {
     for (var notifier in _valueListenableSubscriptions.keys) {
       for (var listener in _valueListenableSubscriptions[notifier]!) {
         notifier.removeListener(listener);
       }
     }
   }
+
+  @Deprecated("use removeValueListeners")
+  void removeListeners() => cancelValueListeners;
 }
