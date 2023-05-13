8866fc1953ddccb89eff39456b862f4f4b40bea9
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2023-05-13 23:57:02 +0300

    no message

--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-typedef TypedCallback<T> = void Function(T value);
+typedef TypedCallback<T> = ValueSetter<T>;
+typedef TypedCallbackP2<P1, P2> = void Function(P1 param1, P2 param2);
+typedef TypedCallbackP3<P1, P2, P3> = void Function(P1 param1, P2 param2, P3 param3);
+
 typedef AsyncTypedCallback<F, T> = Future<F> Function(T value);
-typedef TypedResult<R> = R Function();
-typedef AsyncTypedResult<R> = Future<R> Function();
+typedef TypedResult<R> = ValueGetter<R>;
+typedef TypedResultR2<R1, R2> = (R1, R2) Function();
+
+typedef AsyncTypedResult<R> = AsyncValueGetter<R>;
 typedef TypedResultCallback<R, T> = R Function(T value);
 typedef AsyncTypedResultCallback<R, T> = Future<R> Function(T value);
 
