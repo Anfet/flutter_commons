efbcc2a36b5f58dfe793597efd397f2b9e33842c
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2023-05-13 09:52:11 +0300

    no message

--- lib/src/extensions/datetime_ext.dart
+++ lib/src/extensions/datetime_ext.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/range.dart';
+
 extension SiberitanDateTimeExt on DateTime {
   bool isSameOrAfter(DateTime other) => this.millisecondsSinceEpoch >= other.millisecondsSinceEpoch;
 
@@ -16,4 +18,6 @@ extension SiberitanDateTimeExt on DateTime {
   DateTime get stripTime => DateTime(year, month, day, 0, 0, 0, 0, 0);
 
   bool between(DateTime a, DateTime b) => isSameOrAfter(a) && isSameOrBefore(b);
+
+  bool betweenRange(Range<DateTime> range) => isSameOrAfter(range.requireFrom) && isSameOrBefore(range.requireTill);
 }
