39fd6d6d04bfc3eb7486ed9ea75fb9fb4041de15
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2023-11-13 23:50:17 +0200

    * обновлены core библиотеки * в value notifier маппер добавлена функция dispose

--- lib/src/extensions/value_notifier_ext.dart
+++ lib/src/extensions/value_notifier_ext.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/foundation.dart';
 
 extension ValueListenerExt<T> on ValueListenable<T> {
-  ValueListenable<X> mapValue<X>(X Function(T it) block) => ValueNotifierMapper<T, X>(source: this, mapper: block);
+  ///maps a value from source listenable to another via a mapper function.
+  ///- [mapper] transformation function
+  ///- [onDispose] is called when the resulting mapper is being disposed. Default implementation will remove the mapper listener,
+  ///but you may wish to dispose the source listenable too
+  ValueListenable<X> mapValue<X>(X Function(T it) mapper, {VoidCallback? onDispose}) => ValueNotifierMapper<T, X>(
+        source: this,
+        mapper: mapper,
+      );
 }
 
 class ValueNotifierMapper<T, X> extends ValueNotifier<X> {
   final ValueListenable<T> source;
   final X Function(T value) mapper;
+  final VoidCallback? onDispose;
 
-  ValueNotifierMapper({required this.source, required this.mapper}) : super(mapper(source.value)) {
+  ValueNotifierMapper({required this.source, required this.mapper, this.onDispose}) : super(mapper(source.value)) {
     source.addListener(onValueChanged);
   }
 
@@ -19,6 +27,7 @@ class ValueNotifierMapper<T, X> extends ValueNotifier<X> {
   @override
   void dispose() {
     source.removeListener(onValueChanged);
+    onDispose?.call();
     super.dispose();
   }
 }
