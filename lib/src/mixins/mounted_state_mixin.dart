b6f4ad0bbf4c910b4aa4cc627e33d51f9152c507
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2024-04-03 00:05:01 +0300

    * изменения в mountedStateMixin

--- lib/src/mixins/mounted_state_mixin.dart
+++ lib/src/mixins/mounted_state_mixin.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/cupertino.dart';
 
 mixin MountedStateMixin<S extends StatefulWidget> on State<S> {
+  bool _disposed = false;
+
+  @override
+  void dispose() {
+    _disposed = true;
+    super.dispose();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    if (mounted && !_disposed) {
+      super.setState(fn);
+    }
+  }
+
+  /**
+   * Запрашивает обновление состояния. Использовать можно, если изменения провоцируют переменные, которые вставлять в setState неразумно
+   */
+  void markNeedsRebuild() => setState(() { });
+
+  @Deprecated('Устарело. Лучше использовать setState')
   void setStateChecked(VoidCallback func) {
-    if (mounted) {
-      setState(() => func());
+    if (mounted && !_disposed) {
+      setState(func);
     }
   }
 }
