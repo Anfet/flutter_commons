79649e4b04439c228e52d83cbb48cddf26a1da52
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2025-11-19 19:50:34 +0200

    - added future extensions on futures for lists

--- lib/src/extensions/future_ext.dart
+++ lib/src/extensions/future_ext.dart
@@ -2,6 +2,16 @@ extension SiberianFutureIterableExt<T> on Future<Iterable<T>> {
   Future<List<T>> toList() => then((it) => it.toList());
 
   Future<Iterable<X>> map<X>(X Function(T item) mapper) => then((iterable) => iterable.map(mapper));
+
+  Future<List<X>> mapList<X>(X Function(T item) mapper) => then((iterable) => iterable.map(mapper).toList());
+
+  Future<T?> get firstOrNull => then((iterable) => iterable.firstOrNull);
+
+  Future<T?> get lastOrNull => then((iterable) => iterable.lastOrNull);
+
+  Future<T> get first => then((iterable) => iterable.first);
+
+  Future<T> get last => then((iterable) => iterable.last);
 }
 
 extension LibFutureMapExt<K, V> on Future<Map<K, V>> {
